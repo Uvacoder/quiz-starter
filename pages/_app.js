dbaee68bee5e200195c522abe46c91d687cb1305
Vojta Holik <vojta@egghead.io>
2020-10-09 16:32:37 +0200

    rm excalidraw css

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import React from 'react'
 // import '../node_modules/excalidraw/dist/excalidraw.min.css'
 // import 'excalidraw/dist/excalidraw.min.css'
-import '../styles/excalidraw.css'
+// import '../styles/excalidraw.css'
 
 const App = ({Component, pageProps}) => {
   return (
